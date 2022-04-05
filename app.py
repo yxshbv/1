e862d496c5dec8240ee82ba8f5f3533abf41ee40
yxshbv <elya2204yakh@mail.ru>
2022-04-05 16:19:57 +0300

    серединка

--- app.py
+++ app.py
@@ -5,3 +5,9 @@ if m == 1 or m == 2 or m == 12:
 
 elif m == 3 or m == 4 or m == 5:
     print("Весна")
+
+elif m == 6 or m == 7 or m == 8:
+    print("Лето")
+
+elif m == 9 or m == 10 or m == 11:
+    print("Осень")
